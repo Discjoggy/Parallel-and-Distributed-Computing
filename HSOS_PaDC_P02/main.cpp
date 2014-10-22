482e17ca307d3d0adff9d76ca79f72f997ef486e
Discjoggy <Discjoggy@users.noreply.github.com>
2014-10-22 22:22:00 +0200

    Create main.cpp

---
+++ HSOS_PaDC_P02/main.cpp
@@ -0,0 +1,95 @@
+//============================================================================
+// Name        : Main.cpp
+// Author      : Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
+// Version     : 1.00
+// Copyright   : GPLv3
+// Description : Sieb des Eratosthenes. Umsetzung mit tbb.
+//============================================================================
+
+#include <iostream>
+#include <tbb/blocked_range.h>
+#include <tbb/concurrent_vector.h>
+#include <tbb/parallel_for.h>
+#include <tbb/tick_count.h>
+
+#define DEBUG 0
+#define N 1 << 20
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace tbb;
+
+void print_vector(concurrent_vector<bool> &primes, bool all = false) {
+	for (ull i = 0; i < N; i++) {
+		if (primes[i]) {
+			std::cout << i + 2 << " ";
+		}
+		else if (all) {
+			std::cout << i + 2 << " ";
+		}
+	}
+}
+
+void eliminate_primes(concurrent_vector<bool> &primes, ull i) {
+	if (primes[i - 2]) {
+		for (auto j = 2 * i; j <= N; j += i) {
+			primes[j - 2] = false;
+		}
+	}
+}
+
+void clear_and_initialize_primes(concurrent_vector<bool> &primes) {
+	primes.clear();
+	for (ull i = 2; i < N; i++) {
+		primes.push_back(true);
+	}
+}
+
+/**
+ * Funktions-Objekt zur Parallelisierung des Sieb des Eratosthenes.
+ */
+class ParallelEratosthenes {
+	concurrent_vector<bool> &primes;
+public:
+	ParallelEratosthenes(concurrent_vector<bool> &primes) : primes(primes) {}
+    void operator() (const blocked_range<ull>& range) const {
+    	for (ull i = range.begin(); i * i <= N; i++) {
+    		eliminate_primes(primes, i);
+    	}
+    }
+};
+
+/**
+ * Main-Methode.
+ */
+int main(int argc, char** argv) {
+	tick_count start, end;
+	concurrent_vector<bool> primes;
+
+
+	// Primes (Sequential)
+	clear_and_initialize_primes(primes);
+	start = tick_count::now();
+	for (ull i = 2; i * i <= N; i++) {
+		eliminate_primes(primes, i);
+	}
+	end = tick_count::now();
+#if DEBUG
+	print_vector(primes, 0);
+#endif
+	std::cout << std::endl << "Sequential:\t" << (end - start).seconds() << " s" << std::endl << std::endl;
+
+
+	// Primes (Parallel)
+	clear_and_initialize_primes(primes);
+	start = tick_count::now();
+	parallel_for(blocked_range<ull>(2, N), ParallelEratosthenes(primes));
+	end = tick_count::now();
+#if DEBUG
+	print_vector(primes, 0);
+#endif
+	std::cout << std::endl << "Parallel:\t" << (end - start).seconds() << " s" << std::endl << std::endl;
+
+    return 0;
+}
