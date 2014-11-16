81f6f691d40fcf039bfff1b82d4bc5c03b05a81b
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-11-16 19:10:45 +0100

    Update Main.cpp

--- HSOS_PaDC_Strassen/Main.cpp
+++ HSOS_PaDC_Strassen/Main.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : Main.cpp
+// Name        : Strassen.cpp
 // Author      : Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
 //				 Jens Overmoeller <Jens.Overmoeller@HS-Osnabrueck.de>
 // Version     : 1.00
@@ -8,77 +8,396 @@
 // Description : Algorithmus: Strassen. Parallelisierung mit tbb.
 //============================================================================
 
-#include "Definitions.h"
-#include "Helper.h"
-#include "Log.h"
-#include "Matrix.h"
 #include "Strassen.h"
+#include "Helper.h"
+#include "Matrix.h"
 #include <vector>
 #include <tbb/blocked_range2d.h>
 #include <tbb/parallel_for.h>
-#include <tbb/task_scheduler_init.h>
-#include <tbb/tick_count.h>
-
-using namespace tbb;
 
 /**
-*  @brief  Main-Methode zum Ausfuehren der verschiedenen Algorithmen.
+*  @brief  Konstruktor der Strassen-Klasse.
+*  @param  __A  Matrix A.
+*  @param  __B  Matrix B.
+*  @param  __C  Matrix C (Ergebnismatrix).
+*  @param  __n  Matrixdimension (NxN).
 */
-int main(/*int argc, char* argv[]*/) {
-	Log::ReportingLevel() = logDEBUG4;
-	tick_count t0, t1;
+Strassen::Strassen(Matrix& __A, Matrix& __B, Matrix& __C, size_t __n) : A(__A), B(__B), C(__C), n(__n) {
+}
 
-#if USE_SPECIFIC_THREAD_COUNT
-	LOG(logDEBUG) << "Using " << NO_THREADS << " threads";
-	tbb::task_scheduler_init init(NO_THREADS);
+/**
+*  @brief  Vererbte und ueberschriebene Methode der tbb::task-Klasse,
+*  welche die erbende Klasse in einem Task ausfuehren laesst. Der
+*  Strassen-Algorithmus wird in dieser Methode rekursiv ausgefuerht.
+*/
+tbb::task* Strassen::execute() {
+	size_t newN = n >> 1;
+
+	if (n <= SEQ_SIZE) {
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixMultSeq(A, B, C, 0, n, 0, n, 0, n);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, n, 0, n), MatrixMultPBody(A, B, C, 0, n));
+#endif
+	}
+	else {
+		// Devide & Conquer
+		Matrix A11(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix A12(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix A21(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix A22(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+
+		Matrix B11(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix B12(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix B21(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix B22(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+
+		Matrix C11(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix C12(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix C21(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix C22(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+
+		for (size_t i = 0; i < newN; ++i) {
+			size_t iPlusNewN = i + newN;
+			for (size_t j = 0; j < newN; ++j) {
+				size_t jPlusNewN = j + newN;
+				A11[i][j] = A[i][j];
+				A12[i][j] = A[i][jPlusNewN];
+				A21[i][j] = A[iPlusNewN][j];
+				A22[i][j] = A[iPlusNewN][jPlusNewN];
+
+				B11[i][j] = B[i][j];
+				B12[i][j] = B[i][jPlusNewN];
+				B21[i][j] = B[iPlusNewN][j];
+				B22[i][j] = B[iPlusNewN][jPlusNewN];
+
+				C11[i][j] = C[i][j];
+				C12[i][j] = C[i][jPlusNewN];
+				C21[i][j] = C[iPlusNewN][j];
+				C22[i][j] = C[iPlusNewN][jPlusNewN];
+			}
+		}
+
+
+		// M2 = (A21 + A22) * B11
+		Matrix M2(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp1M2(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixAddSeq(A21, A22, tmp1M2, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(A21, A22, tmp1M2));
+#endif
+		spawn(*new (allocate_child()) Strassen(tmp1M2, B11, M2, newN));
+
+		// M3 = A11 * (B12 - B22)
+		Matrix M3(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp1M3(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixSubSeq(B12, B22, tmp1M3, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(B12, B22, tmp1M3));
+#endif
+		spawn(*new (allocate_child()) Strassen(A11, tmp1M3, M3, newN));
+
+		// M4 = A22 * (B21 - B11)
+		Matrix M4(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp1M4(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixSubSeq(B21, B11, tmp1M4, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(B21, B11, tmp1M4));
+#endif
+		spawn(*new (allocate_child()) Strassen(A22, tmp1M4, M4, newN));
+
+		// M5 = (A11 + A12) * B22128
+		Matrix M5(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp1M5(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixAddSeq(A11, A12, tmp1M5, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(A11, A12, tmp1M5));
+#endif
+		set_ref_count(5);
+		spawn_and_wait_for_all(*new (allocate_child()) Strassen(tmp1M5, B22, M5, newN));
+
+		for (size_t i = 0; i < newN; ++i) {
+			for (size_t j = 0; j < newN; ++j) {
+				C11[i][j] = M4[i][j] - M5[i][j];
+				C12[i][j] = M3[i][j] + M5[i][j];
+				C21[i][j] = M2[i][j] + M4[i][j];
+				C22[i][j] = M3[i][j] - M2[i][j];
+			}
+		}
+
+		M2.clear(); M2.shrink_to_fit();
+		M3.clear(); M3.shrink_to_fit();
+		M4.clear(); M4.shrink_to_fit();
+		M5.clear(); M5.shrink_to_fit();
+		tmp1M2.clear(); tmp1M2.shrink_to_fit();
+		tmp1M3.clear(); tmp1M3.shrink_to_fit();
+		tmp1M4.clear(); tmp1M4.shrink_to_fit();
+		tmp1M5.clear(); tmp1M5.shrink_to_fit();
+
+
+		// M1 = (A11 + A22) * (B11 + B22)
+		Matrix M1(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp1M1(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp2M1(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixAddSeq(A11, A22, tmp1M1, newN);
+		MatrixAddSeq(B11, B22, tmp2M1, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(A11, A22, tmp1M1));
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(B11, B22, tmp2M1));
+#endif
+		spawn(*new (allocate_child()) Strassen(tmp1M1, tmp2M1, M1, newN));
+
+		// M6 = (A21 - A11) * (B11 + B12)
+		Matrix M6(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp1M6(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp2M6(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixSubSeq(A21, A11, tmp1M6, newN);
+		MatrixAddSeq(B11, B12, tmp2M6, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(A21, A11, tmp1M6));
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(B11, B12, tmp2M6));
+#endif
+		spawn(*new (allocate_child()) Strassen(tmp1M6, tmp2M6, M6, newN));
+
+		// M7 = (A12 - A22) * (B21 + B22)
+		Matrix M7(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp1M7(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix tmp2M7(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixSubSeq(A12, A22, tmp1M7, newN);
+		MatrixAddSeq(B21, B22, tmp2M7, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(A12, A22, tmp1M7));
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(B21, B22, tmp2M7));
+#endif
+		set_ref_count(4);
+		spawn_and_wait_for_all(*new (allocate_child()) Strassen(tmp1M7, tmp2M7, M7, newN));
+
+		for (size_t i = 0; i < newN; ++i) {
+			for (size_t j = 0; j < newN; ++j) {
+				C11[i][j] += M1[i][j] + M7[i][j];
+				C22[i][j] += M1[i][j] + M6[i][j];
+			}
+		}
+
+#if USE_SEQ_IN_STRASSEN_TASKS
+		MatrixIncreaseAndCopySeq(C, C11, C12, C21, C22, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixIncreaseAndCopyBody(C, C11, C12, C21, C22, newN));
 #endif
 
-	Matrix A(std::vector<std::vector<M_VAL_TYPE> >(SIZE, std::vector<M_VAL_TYPE>(SIZE, 0)));
-	Matrix B(std::vector<std::vector<M_VAL_TYPE> >(SIZE, std::vector<M_VAL_TYPE>(SIZE, 0)));
-	Matrix C1(std::vector<std::vector<M_VAL_TYPE> >(SIZE, std::vector<M_VAL_TYPE>(SIZE, 0)));
-	Matrix C2(std::vector<std::vector<M_VAL_TYPE> >(SIZE, std::vector<M_VAL_TYPE>(SIZE, 0)));
+		M1.clear(); M1.shrink_to_fit();
+		M6.clear(); M6.shrink_to_fit();
+		M7.clear(); M7.shrink_to_fit();
 
-	initRandomizer();
-	initializeRandpriomMatrix(A, SIZE);
-	initializeRandpriomMatrix(B, SIZE);
+		A11.clear(); A11.shrink_to_fit();
+		A12.clear(); A12.shrink_to_fit();
+		A21.clear(); A21.shrink_to_fit();
+		A22.clear(); A22.shrink_to_fit();
 
-	printMatrix(A, "A");
-	printMatrix(B, "B");
+		B11.clear(); B11.shrink_to_fit();
+		B12.clear(); B12.shrink_to_fit();
+		B21.clear(); B21.shrink_to_fit();
+		B22.clear(); B22.shrink_to_fit();
 
-//	// Naiv
-//	resetValuesMatrix(C2, SIZE);
-//	t0 = tick_count::now();
-//	MatrixMultSeq(A, B, C2, 0, SIZE, 0, SIZE, 0, SIZE);
-//	printMatrix(C2, "C2 = A * B");
-//	t1 = tick_count::now();
-//	LOG(logINFO) << "SMULT: Time was " << (t1 - t0).seconds() << "s" << " - Naiv";
+		C11.clear(); C11.shrink_to_fit();
+		C12.clear(); C12.shrink_to_fit();
+		C21.clear(); C21.shrink_to_fit();
+		C22.clear(); C22.shrink_to_fit();
 
-	// Naiv-Parallel
-	resetValuesMatrix(C2, SIZE);
-	t0 = tick_count::now();
-	parallel_for(blocked_range2d<size_t>(0, SIZE, 0, SIZE), MatrixMultPBody(A, B, C2, 0, SIZE));
-	printMatrix(C2, "C2 = A * B");
-	t1 = tick_count::now();
-	LOG(logINFO) << "PMULT: Time was " << (t1 - t0).seconds() << "s" << " - Naiv-Parallel";
+		tmp1M1.clear(); tmp1M1.shrink_to_fit();
+		tmp1M6.clear(); tmp1M6.shrink_to_fit();
+		tmp1M7.clear(); tmp1M7.shrink_to_fit();
+		tmp2M1.clear(); tmp2M1.shrink_to_fit();
+		tmp2M6.clear(); tmp2M6.shrink_to_fit();
+		tmp2M7.clear(); tmp2M7.shrink_to_fit();
+ 	}
 
-	// Strassen-Algorithmus: Non-Tasks
-	resetValuesMatrix(C1, SIZE);
-	t0 = tick_count::now();
-	strassenRecursive(A, B, C1, SIZE);
-	printMatrix(C1, "C1 = A * B");
-	t1 = tick_count::now();
-	LOG(logINFO) << "RSMULT: Time was " << (t1 - t0).seconds() << "s"<< " - Strassen-Alg.: Non-Tasks";
-	compareMatrices(C1, C2, SIZE) ? std::cout << "RSMULT: Matrizen ungleich\n" : std::cout << "RSMULT: OK\n";
+	return NULL;
+}
 
-	// Strassen-Algorithmus: Tasks
-	resetValuesMatrix(C1, SIZE);
-	t0 = tick_count::now();
-	task::spawn_root_and_wait(*new (task::allocate_root()) Strassen(A, B, C1, SIZE));
-	printMatrix(C1, "C1 = A * B");
-	t1 = tick_count::now();
-	LOG(logINFO) << "PSMULT: Time was " << (t1 - t0).seconds() << "s" << " - Strassen-Alg.: Tasks";
-	compareMatrices(C1, C2, SIZE) ? std::cout << "PSMULT: Matrizen ungleich\n" : std::cout << "PSMULT: OK\n";
+/**
+*  @brief  Rekursive Methode, welche zwei Matrizen nach dem
+*  Strassen-Algorithmus errechnet.
+*  @param  A  Matrix A.
+*  @param  B  Matrix B.
+*  @param  C  Matrix C (Ergebnismatrix).
+*  @param  n  Matrixdimension (NxN).
+*/
+void strassenRecursive(Matrix& A, Matrix& B, Matrix& C, size_t n) {
+	size_t newN = n >> 1;
 
-	std::cout << "\n\nEND\n" ;
-	return EXIT_SUCCESS;
+	if (n <= SEQ_SIZE) {
+#if USE_SEQ_IN_STRASSEN
+		MatrixMultSeq(A, B, C, 0, n, 0, n, 0, n);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, n, 0, n), MatrixMultPBody(A, B, C, 0, n));
+#endif
+	}
+	else {
+		// Devide & Conquer
+		Matrix A11(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix A12(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix A21(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix A22(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+
+		Matrix B11(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix B12(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix B21(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix B22(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+
+		Matrix C11(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix C12(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix C21(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix C22(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+
+		for (size_t i = 0; i < newN; ++i) {
+			size_t iPlusNewN = i + newN;
+			for (size_t j = 0; j < newN; ++j) {
+				size_t jPlusNewN = j + newN;
+				A11[i][j] = A[i][j];
+				A12[i][j] = A[i][jPlusNewN];
+				A21[i][j] = A[iPlusNewN][j];
+				A22[i][j] = A[iPlusNewN][jPlusNewN];
+
+				B11[i][j] = B[i][j];
+				B12[i][j] = B[i][jPlusNewN];
+				B21[i][j] = B[iPlusNewN][j];
+				B22[i][j] = B[iPlusNewN][jPlusNewN];
+
+				C11[i][j] = C[i][j];
+				C12[i][j] = C[i][jPlusNewN];
+				C21[i][j] = C[iPlusNewN][j];
+				C22[i][j] = C[iPlusNewN][jPlusNewN];
+			}
+		}
+
+
+		// M2 = (A21 + A22) * B11
+		Matrix tmp1(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix M2(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN
+		MatrixAddSeq(A21, A22, tmp1, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(A21, A22, tmp1));
+#endif
+		strassenRecursive(tmp1, B11, M2, newN);
+
+		// M3 = A11 * (B12 - B22)
+		Matrix M3(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN
+		MatrixSubSeq(B12, B22, tmp1, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(B12, B22, tmp1));
+#endif
+		strassenRecursive(A11, tmp1, M3, newN);
+
+		// M4 = A22 * (B21 - B11)
+		Matrix M4(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN
+		MatrixSubSeq(B21, B11, tmp1, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(B21, B11, tmp1));
+#endif
+		strassenRecursive(A22, tmp1, M4, newN);
+
+		// M5 = (A11 + A12) * B22128
+		Matrix M5(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN
+		MatrixAddSeq(A11, A12, tmp1, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(A11, A12, tmp1));
+#endif
+		strassenRecursive(tmp1, B22, M5, newN);
+
+		for (size_t i = 0; i < newN; ++i) {
+			for (size_t j = 0; j < newN; ++j) {
+				C11[i][j] = M4[i][j] - M5[i][j];
+				C12[i][j] = M3[i][j] + M5[i][j];
+				C21[i][j] = M2[i][j] + M4[i][j];
+				C22[i][j] = M3[i][j] - M2[i][j];
+			}
+		}
+
+		M2.clear(); M2.shrink_to_fit();
+		M3.clear(); M3.shrink_to_fit();
+		M4.clear(); M4.shrink_to_fit();
+		M5.clear(); M5.shrink_to_fit();
+
+
+		// M1 = (A11 + A22) * (B11 + B22)
+		Matrix tmp2(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+		Matrix M1(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN
+		MatrixAddSeq(A11, A22, tmp1, newN);
+		MatrixAddSeq(B11, B22, tmp2, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(A11, A22, tmp1));
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(B11, B22, tmp2));
+#endif
+		strassenRecursive(tmp1, tmp2, M1, newN);
+
+		// M6 = (A21 - A11) * (B11 + B12)
+		Matrix M6(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN
+		MatrixSubSeq(A21, A11, tmp1, newN);
+		MatrixAddSeq(B11, B12, tmp2, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(A21, A11, tmp1));
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(B11, B12, tmp2));
+#endif
+		strassenRecursive(tmp1, tmp2, M6, newN);
+
+		// M7 = (A12 - A22) * (B21 + B22)
+		Matrix M7(std::vector< std::vector<M_VAL_TYPE> >(newN, std::vector<M_VAL_TYPE>(newN, 0)));
+#if USE_SEQ_IN_STRASSEN
+		MatrixSubSeq(A12, A22, tmp1, newN);
+		MatrixAddSeq(B21, B22, tmp2, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixSubPBody(A12, A22, tmp1));
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixAddPBody(B21, B22, tmp2));
+#endif
+		strassenRecursive(tmp1, tmp2, M7, newN);
+
+		for (size_t i = 0; i < newN; ++i) {
+			for (size_t j = 0; j < newN; ++j) {
+				C11[i][j] += M1[i][j] + M7[i][j];
+				C22[i][j] += M1[i][j] + M6[i][j];
+			}
+		}
+
+#if USE_SEQ_IN_STRASSEN
+		MatrixIncreaseAndCopySeq(C, C11, C12, C21, C22, newN);
+#else
+		tbb::parallel_for(tbb::blocked_range2d<size_t>(0, newN, 0, newN), MatrixIncreaseAndCopyBody(C, C11, C12, C21, C22, newN));
+#endif
+
+		M1.clear(); M1.shrink_to_fit();
+		M6.clear(); M6.shrink_to_fit();
+		M7.clear(); M7.shrink_to_fit();
+
+		A11.clear(); A11.shrink_to_fit();
+		A12.clear(); A12.shrink_to_fit();
+		A21.clear(); A21.shrink_to_fit();
+		A22.clear(); A22.shrink_to_fit();
+
+		B11.clear(); B11.shrink_to_fit();
+		B12.clear(); B12.shrink_to_fit();
+		B21.clear(); B21.shrink_to_fit();
+		B22.clear(); B22.shrink_to_fit();
+
+		C11.clear(); C11.shrink_to_fit();
+		C12.clear(); C12.shrink_to_fit();
+		C21.clear(); C21.shrink_to_fit();
+		C22.clear(); C22.shrink_to_fit();
+
+		tmp1.clear(); tmp1.shrink_to_fit();
+		tmp2.clear(); tmp2.shrink_to_fit();
+	}
 }
