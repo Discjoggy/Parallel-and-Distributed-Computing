d063decde828237dcf87bb42136d6831bf279fe9
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-11-20 22:04:45 +0100

    Create Definitions.h

---
+++ HSOS_PaDC_P03/Definitions.h
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Definitions.h
+// Author      : Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
+//				 Jens Overmoeller <Jens.Overmoeller@HS-Osnabrueck.de>
+// Version     : 1.00
+// Copyright   : GPLv3
+// Created on  : 17.11.2014
+// Description : Algorithmus: Langford. Parallelisierung mit tbb.
+//============================================================================
+
+#ifndef DEFINITIONS_H_
+#define DEFINITIONS_H_
+
+#define DEBUG 1							// Debuggen? (z.B. Verwendung von Consolen-Ausgaben)
+
+#define NO_THREADS 4					// Anzahl zu nutzender Threads (Falls aktiviert)
+#define USE_SPECIFIC_THREAD_COUNT 0		// Soll spezifische Thread-Anzahl genutzt werden?
+
+#endif /* DEFINITIONS_H_ */
