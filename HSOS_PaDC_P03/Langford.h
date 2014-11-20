9d910db66056eb284c2cacbc9bdfe3f3a738e07d
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-11-20 22:03:59 +0100

    Create Langford.h

---
+++ HSOS_PaDC_P03/Langford.h
@@ -0,0 +1,41 @@
+//============================================================================
+// Name        : Langford.h
+// Author      : Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
+// Version     : 1.00
+// Copyright   : GPLv3
+// Created on  : 17.11.2014
+// Description : Algorithmus: Langford. Parallelisierung mit tbb.
+//============================================================================
+
+#ifndef LANGFORD_H_
+#define LANGFORD_H_
+
+#include "Definitions.h"
+#include <iostream>
+#include <tbb/atomic.h>
+#include <tbb/task.h>
+
+size_t nextClearBit(bool* arr, size_t n, size_t i);
+
+void print(bool* arr, size_t n);
+
+void LangfordRecursive(bool* __arr, size_t lvl, size_t& size, size_t& count);
+
+void LangfordRecursiveBit(size_t tree, size_t lvl, size_t& size, size_t& count);
+
+/**
+*  @brief  Repraesentiert eine Klasse, welche das Langford-
+*  Problem rekursiv löst.
+*/
+class Langford : public tbb::task {
+	size_t tree;
+	size_t lvl;
+	size_t& size;
+	tbb::atomic<size_t>& count;
+
+public:
+	Langford(size_t __tree, size_t __lvl, size_t& __size, tbb::atomic<size_t>& __count);
+	tbb::task* execute();
+};
+
+#endif /* LANGFORD_H_ */
