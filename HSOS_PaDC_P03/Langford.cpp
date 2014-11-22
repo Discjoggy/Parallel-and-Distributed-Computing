7f68f11049be208cd76ca372ff29df448a092d38
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-11-22 02:08:16 +0100

    Update Langford.cpp

--- HSOS_PaDC_P03/Langford.cpp
+++ HSOS_PaDC_P03/Langford.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : Langford.cpp
+// Name        : Langford.h
 // Author      : Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
 // Version     : 1.00
 // Copyright   : GPLv3
@@ -7,125 +7,40 @@
 // Description : Algorithmus: Langford. Parallelisierung mit tbb.
 //============================================================================
 
-#include "Langford.h"
+#ifndef LANGFORD_H_
+#define LANGFORD_H_
+
+#include "Definitions.h"
+#include <iostream>
+#include <tbb/atomic.h>
+#include <tbb/task.h>
+#include <vector>
+
+void printBits(size_t& x, size_t& size);
+
+size_t reverse(size_t x, size_t size);
+
+size_t nextClearBit(bool* arr, size_t n, size_t i);
+
+void print(bool* arr, size_t n);
+
+void LangfordRecursive(bool* __arr, size_t lvl, size_t& size, size_t& count);
+
+void LangfordRecursiveBit(size_t tree, size_t lvl, size_t& size, size_t& count);
 
 /**
-*  @brief  Gibt das uebergebene Array aus.
-*  @param  __arr  Array
-*  @param  __n    Groeße des Arrays
+*  @brief  Repraesentiert eine Klasse, welche das Langford-
+*  Problem rekursiv löst.
 */
-void print(bool* arr, size_t n) {
-	std::cout << "\n[ ";
-	for (size_t i = 0; i < n; ++i) {
-		std::cout << arr[i] << " ";
-	}
-	std::cout << "]";
-}
+class Langford : public tbb::task {
+	size_t tree;
+	size_t lvl;
+	size_t& size;
+	tbb::atomic<size_t>& count;
 
-/**
-*  @brief  Gibt das naechstfreie false-Bit zurueck.
-*  @param  __arr    Array
-*  @param  __n      Groeße des Arrays
-*  @param  __index  Startindex (optional)
-*/
-size_t nextClearBit(bool* arr, size_t n, size_t index = 0) {
-	for (size_t i = index; i < n; ++i) {
-		if (!arr[i]) {
-			return i;
-		}
-	}
-	return n;
-}
+public:
+	Langford(size_t __tree, size_t __lvl, size_t& __size, tbb::atomic<size_t>& __count);
+	tbb::task* execute();
+};
 
-/**
-*  @brief  Langford-Algorithmus (Bit-Shiftig).
-*  @param  __arr    Boolsches Array
-*  @param  __lvl    SB-Ebene
-*  @param  __size   Groeße
-*  @param  __count  Globaler zaehler
-*/
-void LangfordRecursiveBit(size_t tree, size_t lvl, size_t& size, size_t& count) {
-	if (lvl < 1) {
-		++count;
-	}
-	else {
-		size_t realEnd = size - lvl - 1;
-		for (size_t i = 0; i < realEnd; ++i) {
-			size_t j = i + lvl + 1;
-			if (!(tree & (1 << i)) && !(tree & (1 << j)))
-			{
-				size_t treeTree = (1 << i) + (1 << j);
-				LangfordRecursiveBit( tree + treeTree, lvl - 1, size, count);
-			}
-		}
-	}
-}
-
-/**
-*  @brief  Langford-Algorithmus.
-*  @param  __arr    Boolsches Array
-*  @param  __lvl    SB-Ebene
-*  @param  __size   Groeße
-*  @param  __count  Globaler zaehler
-*/
-void LangfordRecursive(bool* arr, size_t lvl, size_t& size, size_t& count) {
-	if (lvl < 1) {
-		for (size_t i = 0; i < size; ++i) {
-			if (!arr[i]) {
-				return;
-			}
-		}
-		++count;
-	}
-	else {
-		// Alle möglichen Positionen für Index i und i + Abstand testen
-		for (size_t i = -1; (i = nextClearBit(arr, size, i + 1)) < size - lvl - 1;) {
-			size_t j = i + lvl + 1;
-			if (!arr[j]) {
-				arr[i] = !arr[i];
-				arr[j] = !arr[j];
-				LangfordRecursive(arr, lvl - 1, size, count);
-				arr[i] = !arr[i];
-				arr[j] = !arr[j];
-			}
-		}
-	}
-}
-
-/**
-*  @brief  Langford-Algorithmus. Lösung mithilfe von Tasks.
-*  @param  __tree   Bit-Sequenz
-*  @param  __lvl    SB-Ebene
-*  @param  __size   Groeße
-*  @param  __count  Globaler zaehler
-*/
-Langford::Langford(size_t __tree, size_t __lvl, size_t& __size, tbb::atomic<size_t>& __count) : tree(__tree), lvl(__lvl), size(__size), count(__count) {
-}
-
-/**
-*  @brief  Vererbte und ueberschriebene Methode der tbb::task-Klasse,
-*  welche die erbende Klasse in einem Task ausfuehren laesst.
-*/
-tbb::task* Langford::execute() {
-	if (lvl < 1) {
-		++count;
-	}
-	else {
-		tbb::task_list taskList;
-		size_t taskCount = 0;
-		size_t realEnd = size - lvl - 1;
-		for (size_t i = 0; i < realEnd; ++i)
-		{
-			size_t j = (i + lvl + 1);
-			if (!(tree & (1 << i)) && !(tree & (1 << j)))
-			{
-				size_t treeTree = (1 << i) + (1 << j);
-				taskList.push_back(*new (allocate_child()) Langford(tree + treeTree, lvl - 1, size, count));
-				++taskCount;
-			}
-		}
-		set_ref_count(++taskCount);
-		spawn_and_wait_for_all(taskList);
-	}
-	return NULL;
-}
+#endif /* LANGFORD_H_ */
