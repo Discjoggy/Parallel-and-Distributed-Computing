54efc6ad083320b59ce43ca3de97f76d9b723310
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-12-13 07:13:51 +0100

    Update Definitions.h

--- HSOS_PaDC_Strassen/Definitions.h
+++ HSOS_PaDC_Strassen/Definitions.h
@@ -13,12 +13,14 @@
 
 #include <vector>
 #include <stdint.h>
+#include <tbb/scalable_allocator.h>
 
 typedef uint_fast32_t M_SIZE_TYPE;					// Groessentyp der Matrizen, Schleifenzaehler usw.
 typedef double M_VAL_TYPE;							// Typ der Werte in den Matrizen (Gut: int_least32_t)
-typedef std::vector< std::vector<M_VAL_TYPE> > Matrix;
+typedef std::vector<M_VAL_TYPE, tbb::scalable_allocator<M_VAL_TYPE>> InnerArray;
+typedef std::vector<InnerArray, tbb::scalable_allocator<InnerArray>> Matrix;
 
-#define DEBUG 1										// Debuggen? (Z. B. Verwendung von Consolen-Ausgaben, Konstanten Werten usw.)
+#define DEBUG 0										// Debuggen? (Z. B. Verwendung von Consolen-Ausgaben, Konstanten Werten usw.)
 #define MAX_RAND_VAL RAND_MAX / 50					// Zufallszahlen bis (21474836472147483647 / X) z.B. 50 oder 750
 #define STD_WIDTH 9									// Matrixausgabe: Indexbreite
 #define STD_PRECISION 5								// Matrixausgabe: Genauigkeit bei Gleitkommawerten
@@ -28,8 +30,8 @@ typedef std::vector< std::vector<M_VAL_TYPE> > Matrix;
 #define NO_THREADS 2								// Anzahl zu nutzender Threads (Falls aktiviert)
 #define USE_SPECIFIC_THREAD_COUNT 0					// Soll spezifische Thread-Anzahl genutzt werden?
 
-#define POW 9										// 2^POW
-const M_SIZE_TYPE M_SIZE 		= (2 << POW);			// Dimension der Matrix (SIZE x SIZE)
-const M_SIZE_TYPE CUT_OFF 	= (2 << (POW >> 1));	// Ab welcher Dimension soll naiver Algorithmus eingesetzt werden? Min. CUT_OFF x CUT_OFF = 4!
+#define POW 10										// 2^POW
+const M_SIZE_TYPE M_SIZE	= (2 << POW);			// Dimension der Matrix (SIZE x SIZE)
+const M_SIZE_TYPE CUT_OFF 	= 64;//(2 << (POW >> 1));	// Ab welcher Dimension soll naiver Algorithmus eingesetzt werden? Min. CUT_OFF x CUT_OFF = 4!
 
 #endif
