172d1e341a268366db1b91ab78203e0a380de1bd
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-12-23 06:58:58 +0100

    Update Strassen.cpp

--- HSOS_PaDC_Strassen/Strassen.cpp
+++ HSOS_PaDC_Strassen/Strassen.cpp
@@ -115,115 +115,9 @@ tbb::task* Strassen::execute() {
 				C[iPlusNewN][j + newN] 	+= M2[i][j] + M3[i][j];
 			}
 		}
-
-//		M2.clear(); M3.clear(); M4.clear();
-//		A11.clear(); A12.clear();A21.clear(); A22.clear();
-//		B11.clear(); B12.clear(); B21.clear(); B22.clear();
-//		tmp1M2.clear(); tmp1M3.clear(); tmp1M4.clear(); tmp1M5.clear();
-//		tmp2M4.clear();
-//#if __cplusplus >= 201103L
-//		M2.shrink_to_fit(); M3.shrink_to_fit(); M4.shrink_to_fit();
-//		A11.shrink_to_fit(); A12.shrink_to_fit(); A21.shrink_to_fit(); A22.shrink_to_fit();
-//		B11.shrink_to_fit(); B12.shrink_to_fit(); B21.shrink_to_fit(); B22.shrink_to_fit();
-//		tmp1M2.shrink_to_fit(); tmp1M3.shrink_to_fit(); tmp1M4.shrink_to_fit(); tmp1M5.shrink_to_fit();
-//		tmp2M4.shrink_to_fit();
-//#endif
  	}
 	return NULL;
 }
-/*tbb::task* Strassen::execute() {
-	if (n <= CUT_OFF) {
-		matrixMultSeq(C, A, B, n);
-	}
-	else {
-		const M_SIZE_TYPE newN = n >> 1;
-		// Devide & Conquer
-		Matrix A11(newN, InnerArray(newN));
-		Matrix A12(newN, InnerArray(newN));
-		Matrix A21(newN, InnerArray(newN));
-		Matrix A22(newN, InnerArray(newN));
-
-		Matrix B11(newN, InnerArray(newN));
-		Matrix B12(newN, InnerArray(newN));
-		Matrix B21(newN, InnerArray(newN));
-		Matrix B22(newN, InnerArray(newN));
-
-		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
-			M_SIZE_TYPE iPlusNewN = i + newN;
-			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
-				A11[i][j] = A[i][j];
-				A12[i][j] = A[i][j + newN];
-				A21[i][j] = A[iPlusNewN][j];
-				A22[i][j] = A[iPlusNewN][j + newN];
-
-				B11[i][j] = B[i][j];
-				B12[i][j] = B[i][j + newN];
-				B21[i][j] = B[iPlusNewN][j];
-				B22[i][j] = B[iPlusNewN][j + newN];
-			}
-		}
-
-		// M1 = (A11 + A22) * (B11 + B22)
-		Matrix M1(newN, InnerArray(newN));
-		Matrix tmp1M1(newN, InnerArray(newN));
-		Matrix tmp2M1(newN, InnerArray(newN));
-		matrixAddSeq(tmp1M1, A11, A22, newN);
-		matrixAddSeq(tmp2M1, B11, B22, newN);
-		set_ref_count(8);
-		spawn(*new (allocate_child()) Strassen(M1, tmp1M1, tmp2M1, newN));
-
-		// M2 = (A21 + A22) * B11
-		Matrix M2(newN, InnerArray(newN));
-		Matrix tmp1M2(newN, InnerArray(newN));
-		matrixAddSeq(tmp1M2, A21, A22, newN);
-		spawn(*new (allocate_child()) Strassen(M2, tmp1M2, B11, newN));
-
-		// M3 = A11 * (B12 - B22)
-		Matrix M3(newN, InnerArray(newN));
-		Matrix tmp1M3(newN, InnerArray(newN));
-		matrixSubSeq(tmp1M3, B12, B22, newN);
-		spawn(*new (allocate_child()) Strassen(M3, A11, tmp1M3, newN));
-
-		// M4 = A22 * (B21 - B11)
-		Matrix M4(newN, InnerArray(newN));
-		Matrix tmp1M4(newN, InnerArray(newN));
-		matrixSubSeq(tmp1M4, B21, B11, newN);
-		spawn(*new (allocate_child()) Strassen(M4, A22, tmp1M4, newN));
-
-		// M5 = (A11 + A12) * B22128
-		Matrix M5(newN, InnerArray(newN));
-		Matrix tmp1M5(newN, InnerArray(newN));
-		matrixAddSeq(tmp1M5, A11, A12, newN);
-		spawn(*new (allocate_child()) Strassen(M5, tmp1M5, B22, newN));
-
-		// M6 = (A21 - A11) * (B11 + B12)
-		Matrix M6(newN, InnerArray(newN));
-		Matrix tmp1M6(newN, InnerArray(newN));
-		Matrix tmp2M6(newN, InnerArray(newN));
-		matrixSubSeq(tmp1M6, A21, A11, newN);
-		matrixAddSeq(tmp2M6, B11, B12, newN);
-		spawn(*new (allocate_child()) Strassen(M6, tmp1M6, tmp2M6, newN));
-
-		// M7 = (A12 - A22) * (B21 + B22)
-		Matrix M7(newN, InnerArray(newN));
-		Matrix tmp1M7(newN, InnerArray(newN));
-		Matrix tmp2M7(newN, InnerArray(newN));
-		matrixSubSeq(tmp1M7, A12, A22, newN);
-		matrixAddSeq(tmp2M7, B21, B22, newN);
-		spawn_and_wait_for_all(*new (allocate_child()) Strassen(M7, tmp1M7, tmp2M7, newN));
-
-		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
-			M_SIZE_TYPE iPlusNewN = i + newN;
-			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
-				C[i][j] 				= M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
-				C[i][j + newN]        	= M3[i][j] + M5[i][j];
-				C[iPlusNewN][j]       	= M2[i][j] + M4[i][j];
-				C[iPlusNewN][j + newN] 	= M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
-			}
-		}
-	}
-	return NULL;
-}*/
 
 /**
 *  @brief  Rekursive Methode, welche zwei Matrizen nach dem
@@ -323,16 +217,184 @@ void strassenRecursive(Matrix& C, const Matrix& A, const Matrix& B, const M_SIZE
 				C[iPlusNewN][j + newN] 	+= M2[i][j] + M3[i][j];
 			}
 		}
-
-//		M2.clear(); M3.clear(); M4.clear();
-//		A11.clear(); A12.clear();A21.clear(); A22.clear();
-//		B11.clear(); B12.clear(); B21.clear(); B22.clear();
-//		tmp1.clear(); tmp2.clear(); tmp2.clear();
-//#if __cplusplus >= 201103L
-//		M2.shrink_to_fit(); M3.shrink_to_fit(); M4.shrink_to_fit();
-//		A11.shrink_to_fit(); A12.shrink_to_fit(); A21.shrink_to_fit(); A22.shrink_to_fit();
-//		B11.shrink_to_fit(); B12.shrink_to_fit(); B21.shrink_to_fit(); B22.shrink_to_fit();
-//		tmp1.shrink_to_fit(); tmp2.shrink_to_fit(); tmp2.shrink_to_fit();
-//#endif
 	}
 }
+
+/*tbb::task* Strassen::execute() {
+	if (n <= CUT_OFF) {
+		matrixMultSeq(C, A, B, n);
+	}
+	else {
+		const M_SIZE_TYPE newN = n >> 1;
+		// Devide & Conquer
+		Matrix A11(newN, InnerArray(newN));
+		Matrix A12(newN, InnerArray(newN));
+		Matrix A21(newN, InnerArray(newN));
+		Matrix A22(newN, InnerArray(newN));
+
+		Matrix B11(newN, InnerArray(newN));
+		Matrix B12(newN, InnerArray(newN));
+		Matrix B21(newN, InnerArray(newN));
+		Matrix B22(newN, InnerArray(newN));
+
+		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
+			M_SIZE_TYPE iPlusNewN = i + newN;
+			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
+				A11[i][j] = A[i][j];
+				A12[i][j] = A[i][j + newN];
+				A21[i][j] = A[iPlusNewN][j];
+				A22[i][j] = A[iPlusNewN][j + newN];
+
+				B11[i][j] = B[i][j];
+				B12[i][j] = B[i][j + newN];
+				B21[i][j] = B[iPlusNewN][j];
+				B22[i][j] = B[iPlusNewN][j + newN];
+			}
+		}
+
+		// M1 = (A11 + A22) * (B11 + B22)
+		Matrix M1(newN, InnerArray(newN));
+		Matrix tmp1M1(newN, InnerArray(newN));
+		Matrix tmp2M1(newN, InnerArray(newN));
+		matrixAddSeq(tmp1M1, A11, A22, newN);
+		matrixAddSeq(tmp2M1, B11, B22, newN);
+		set_ref_count(8);
+		spawn(*new (allocate_child()) Strassen(M1, tmp1M1, tmp2M1, newN));
+
+		// M2 = (A21 + A22) * B11
+		Matrix M2(newN, InnerArray(newN));
+		Matrix tmp1M2(newN, InnerArray(newN));
+		matrixAddSeq(tmp1M2, A21, A22, newN);
+		spawn(*new (allocate_child()) Strassen(M2, tmp1M2, B11, newN));
+
+		// M3 = A11 * (B12 - B22)
+		Matrix M3(newN, InnerArray(newN));
+		Matrix tmp1M3(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M3, B12, B22, newN);
+		spawn(*new (allocate_child()) Strassen(M3, A11, tmp1M3, newN));
+
+		// M4 = A22 * (B21 - B11)
+		Matrix M4(newN, InnerArray(newN));
+		Matrix tmp1M4(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M4, B21, B11, newN);
+		spawn(*new (allocate_child()) Strassen(M4, A22, tmp1M4, newN));
+
+		// M5 = (A11 + A12) * B22128
+		Matrix M5(newN, InnerArray(newN));
+		Matrix tmp1M5(newN, InnerArray(newN));
+		matrixAddSeq(tmp1M5, A11, A12, newN);
+		spawn(*new (allocate_child()) Strassen(M5, tmp1M5, B22, newN));
+
+		// M6 = (A21 - A11) * (B11 + B12)
+		Matrix M6(newN, InnerArray(newN));
+		Matrix tmp1M6(newN, InnerArray(newN));
+		Matrix tmp2M6(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M6, A21, A11, newN);
+		matrixAddSeq(tmp2M6, B11, B12, newN);
+		spawn(*new (allocate_child()) Strassen(M6, tmp1M6, tmp2M6, newN));
+
+		// M7 = (A12 - A22) * (B21 + B22)
+		Matrix M7(newN, InnerArray(newN));
+		Matrix tmp1M7(newN, InnerArray(newN));
+		Matrix tmp2M7(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M7, A12, A22, newN);
+		matrixAddSeq(tmp2M7, B21, B22, newN);
+		spawn_and_wait_for_all(*new (allocate_child()) Strassen(M7, tmp1M7, tmp2M7, newN));
+
+		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
+			M_SIZE_TYPE iPlusNewN = i + newN;
+			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
+				C[i][j] 				= M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+				C[i][j + newN]        	= M3[i][j] + M5[i][j];
+				C[iPlusNewN][j]       	= M2[i][j] + M4[i][j];
+				C[iPlusNewN][j + newN] 	= M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+			}
+		}
+	}
+	return NULL;
+}
+
+void strassenRecursive(Matrix& C, const Matrix& A, const Matrix& B, const M_SIZE_TYPE& n) {
+	if (n <= CUT_OFF) {
+		matrixMultSeq(C, A, B, n);
+	}
+	else {
+		const M_SIZE_TYPE newN = n >> 1;
+		// Devide & Conquer
+		Matrix A11(newN, InnerArray(newN));
+		Matrix A12(newN, InnerArray(newN));
+		Matrix A21(newN, InnerArray(newN));
+		Matrix A22(newN, InnerArray(newN));
+
+		Matrix B11(newN, InnerArray(newN));
+		Matrix B12(newN, InnerArray(newN));
+		Matrix B21(newN, InnerArray(newN));
+		Matrix B22(newN, InnerArray(newN));
+
+		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
+			M_SIZE_TYPE iPlusNewN = i + newN;
+			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
+				A11[i][j] = A[i][j];
+				A12[i][j] = A[i][j + newN];
+				A21[i][j] = A[iPlusNewN][j];
+				A22[i][j] = A[iPlusNewN][j + newN];
+
+				B11[i][j] = B[i][j];
+				B12[i][j] = B[i][j + newN];
+				B21[i][j] = B[iPlusNewN][j];
+				B22[i][j] = B[iPlusNewN][j + newN];
+			}
+		}
+
+
+		// M1 = (A11 + A22) * (B11 + B22)
+		Matrix M1(newN, InnerArray(newN));
+		Matrix tmp1(newN, InnerArray(newN));
+		Matrix tmp2(newN, InnerArray(newN));
+		matrixAddSeq(tmp1, A11, A22, newN);
+		matrixAddSeq(tmp2, B11, B22, newN);
+		strassenRecursive(M1, tmp2, tmp1, newN);
+
+		// M2 = (A21 + A22) * B11
+		Matrix M2(newN, InnerArray(newN));
+		matrixAddSeq(tmp1, A21, A22, newN);
+		strassenRecursive(M2, tmp1, B11, newN);
+
+		// M3 = A11 * (B12 - B22)
+		Matrix M3(newN, InnerArray(newN));
+		matrixSubSeq(tmp1, B12, B22, newN);
+		strassenRecursive(M3, A11, tmp1, newN);
+
+		// M4 = A22 * (B21 - B11)
+		Matrix M4(newN, InnerArray(newN));
+		matrixSubSeq(tmp1, B21, B11, newN);
+		strassenRecursive(M4, A22, tmp1, newN);
+
+		// M5 = (A11 + A12) * B22128
+		Matrix M5(newN, InnerArray(newN));
+		matrixAddSeq(tmp1, A11, A12, newN);
+		strassenRecursive(M5, tmp1, B22, newN);
+
+		// M6 = (A21 - A11) * (B11 + B12)
+		Matrix M6(newN, InnerArray(newN));
+		matrixSubSeq(tmp1, A21, A11, newN);
+		matrixAddSeq(tmp2, B11, B12, newN);
+		strassenRecursive(M6, tmp1, tmp2, newN);
+
+		// M7 = (A12 - A22) * (B21 + B22)
+		Matrix M7(newN, InnerArray(newN));
+		matrixSubSeq(tmp1, A12, A22, newN);
+		matrixAddSeq(tmp2, B21, B22, newN);
+		strassenRecursive(M7, tmp1, tmp2, newN);
+
+		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
+			M_SIZE_TYPE iPlusNewN = i + newN;
+			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
+				C[i][j] 				= M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+				C[i][j + newN]        	= M3[i][j] + M5[i][j];
+				C[iPlusNewN][j]       	= M2[i][j] + M4[i][j];
+				C[iPlusNewN][j + newN] 	= M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+			}
+		}
+	}
+}*/
