fae9406aaba571bb682cf64e068e6b5e386a03f1
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-12-23 06:15:03 +0100

    Update Strassen.cpp

--- HSOS_PaDC_Strassen/Strassen.cpp
+++ HSOS_PaDC_Strassen/Strassen.cpp
@@ -53,26 +53,26 @@ tbb::task* Strassen::execute() {
 		Matrix M2(newN, InnerArray(newN));
 		Matrix tmp1M2(newN, InnerArray(newN));
 		matrixAddSeq(tmp1M2, A21, A22, newN);
+		set_ref_count(3);
 		spawn(*new (allocate_child()) Strassen(M2, tmp1M2, B11, newN));
 
 		// M3 = A11 * (B12 - B22)
 		Matrix M3(newN, InnerArray(newN));
 		Matrix tmp1M3(newN, InnerArray(newN));
 		matrixSubSeq(tmp1M3, B12, B22, newN);
-		set_ref_count(3);
 		spawn_and_wait_for_all(*new (allocate_child()) Strassen(M3, A11, tmp1M3, newN));
 
 		// M4 = A22 * (B21 - B11)
 		Matrix M4(newN, InnerArray(newN));
 		Matrix tmp1M4(newN, InnerArray(newN));
 		matrixSubSeq(tmp1M4, B21, B11, newN);
+		set_ref_count(3);
 		spawn(*new (allocate_child()) Strassen(M4, A22, tmp1M4, newN));
 
 		// M5 = (A11 + A12) * B22128
 		Matrix M5(newN, InnerArray(newN));
 		Matrix tmp1M5(newN, InnerArray(newN));
 		matrixAddSeq(tmp1M5, A11, A12, newN);
-		set_ref_count(3);
 		spawn_and_wait_for_all(*new (allocate_child()) Strassen(M5, tmp1M5, B22, newN));
 
 		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
@@ -82,11 +82,7 @@ tbb::task* Strassen::execute() {
 				C[i][j + newN] 			= M3[i][j] + M5[i][j];
 				C[iPlusNewN][j] 		= M2[i][j] + M4[i][j];
 				C[iPlusNewN][j + newN] 	= M3[i][j] - M2[i][j];
-			}
-		}
 
-		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
-			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
 				M2[i][j] = M3[i][j] = M4[i][j] = M5[i][j] = 0;
 			}
 		}
@@ -96,6 +92,7 @@ tbb::task* Strassen::execute() {
 		// Reuse: M1 = M2 | tmp1M1 = tmp1M2 | tmp2M1 = tmp1M5
 		matrixAddSeq(tmp1M2, A11, A22, newN);
 		matrixAddSeq(tmp1M5, B11, B22, newN);
+		set_ref_count(4);
 		spawn(*new (allocate_child()) Strassen(M2, tmp1M2, tmp1M5, newN));
 
 		// M6 = (A21 - A11) * (B11 + B12)
@@ -109,7 +106,6 @@ tbb::task* Strassen::execute() {
 		Matrix tmp2M4(newN, InnerArray(newN));
 		matrixSubSeq(tmp1M4, A12, A22, newN);
 		matrixAddSeq(tmp2M4, B21, B22, newN);
-		set_ref_count(4);
 		spawn_and_wait_for_all(*new (allocate_child()) Strassen(M4, tmp1M4, tmp2M4, newN));
 
 		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
@@ -135,6 +131,99 @@ tbb::task* Strassen::execute() {
  	}
 	return NULL;
 }
+/*tbb::task* Strassen::execute() {
+	if (n <= CUT_OFF) {
+		matrixMultSeq(C, A, B, n);
+	}
+	else {
+		const M_SIZE_TYPE newN = n >> 1;
+		// Devide & Conquer
+		Matrix A11(newN, InnerArray(newN));
+		Matrix A12(newN, InnerArray(newN));
+		Matrix A21(newN, InnerArray(newN));
+		Matrix A22(newN, InnerArray(newN));
+
+		Matrix B11(newN, InnerArray(newN));
+		Matrix B12(newN, InnerArray(newN));
+		Matrix B21(newN, InnerArray(newN));
+		Matrix B22(newN, InnerArray(newN));
+
+		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
+			M_SIZE_TYPE iPlusNewN = i + newN;
+			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
+				A11[i][j] = A[i][j];
+				A12[i][j] = A[i][j + newN];
+				A21[i][j] = A[iPlusNewN][j];
+				A22[i][j] = A[iPlusNewN][j + newN];
+
+				B11[i][j] = B[i][j];
+				B12[i][j] = B[i][j + newN];
+				B21[i][j] = B[iPlusNewN][j];
+				B22[i][j] = B[iPlusNewN][j + newN];
+			}
+		}
+
+		// M1 = (A11 + A22) * (B11 + B22)
+		Matrix M1(newN, InnerArray(newN));
+		Matrix tmp1M1(newN, InnerArray(newN));
+		Matrix tmp2M1(newN, InnerArray(newN));
+		matrixAddSeq(tmp1M1, A11, A22, newN);
+		matrixAddSeq(tmp2M1, B11, B22, newN);
+		set_ref_count(8);
+		spawn(*new (allocate_child()) Strassen(M1, tmp1M1, tmp2M1, newN));
+
+		// M2 = (A21 + A22) * B11
+		Matrix M2(newN, InnerArray(newN));
+		Matrix tmp1M2(newN, InnerArray(newN));
+		matrixAddSeq(tmp1M2, A21, A22, newN);
+		spawn(*new (allocate_child()) Strassen(M2, tmp1M2, B11, newN));
+
+		// M3 = A11 * (B12 - B22)
+		Matrix M3(newN, InnerArray(newN));
+		Matrix tmp1M3(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M3, B12, B22, newN);
+		spawn(*new (allocate_child()) Strassen(M3, A11, tmp1M3, newN));
+
+		// M4 = A22 * (B21 - B11)
+		Matrix M4(newN, InnerArray(newN));
+		Matrix tmp1M4(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M4, B21, B11, newN);
+		spawn(*new (allocate_child()) Strassen(M4, A22, tmp1M4, newN));
+
+		// M5 = (A11 + A12) * B22128
+		Matrix M5(newN, InnerArray(newN));
+		Matrix tmp1M5(newN, InnerArray(newN));
+		matrixAddSeq(tmp1M5, A11, A12, newN);
+		spawn(*new (allocate_child()) Strassen(M5, tmp1M5, B22, newN));
+
+		// M6 = (A21 - A11) * (B11 + B12)
+		Matrix M6(newN, InnerArray(newN));
+		Matrix tmp1M6(newN, InnerArray(newN));
+		Matrix tmp2M6(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M6, A21, A11, newN);
+		matrixAddSeq(tmp2M6, B11, B12, newN);
+		spawn(*new (allocate_child()) Strassen(M6, tmp1M6, tmp2M6, newN));
+
+		// M7 = (A12 - A22) * (B21 + B22)
+		Matrix M7(newN, InnerArray(newN));
+		Matrix tmp1M7(newN, InnerArray(newN));
+		Matrix tmp2M7(newN, InnerArray(newN));
+		matrixSubSeq(tmp1M7, A12, A22, newN);
+		matrixAddSeq(tmp2M7, B21, B22, newN);
+		spawn_and_wait_for_all(*new (allocate_child()) Strassen(M7, tmp1M7, tmp2M7, newN));
+
+		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
+			M_SIZE_TYPE iPlusNewN = i + newN;
+			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
+				C[i][j] 				= M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+				C[i][j + newN]        	= M3[i][j] + M5[i][j];
+				C[iPlusNewN][j]       	= M2[i][j] + M4[i][j];
+				C[iPlusNewN][j + newN] 	= M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+			}
+		}
+	}
+	return NULL;
+}*/
 
 /**
 *  @brief  Rekursive Methode, welche zwei Matrizen nach dem
@@ -205,12 +294,8 @@ void strassenRecursive(Matrix& C, const Matrix& A, const Matrix& B, const M_SIZE
 				C[i][j + newN] 			= M3[i][j] + M5[i][j];
 				C[iPlusNewN][j] 		= M2[i][j] + M4[i][j];
 				C[iPlusNewN][j + newN] 	= M3[i][j] - M2[i][j];
-			}
-		}
 
-		for (M_SIZE_TYPE i = 0; i < newN; ++i) {
-			for (M_SIZE_TYPE j = 0; j < newN; ++j) {
-				 M2[i][j] = M3[i][j] = M4[i][j] = M5[i][j] = 0;
+				M2[i][j] = M3[i][j] = M4[i][j] = M5[i][j] = 0;
 			}
 		}
 
