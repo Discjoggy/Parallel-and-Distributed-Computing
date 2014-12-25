b32f4961c8ee99583398d943ac469ed3ed82a5c6
Tobias Sibera <Discjoggy@users.noreply.github.com>
2014-12-25 14:16:41 +0100

    Update Definitions.h

--- HSOS_PaDC_Strassen/Definitions.h
+++ HSOS_PaDC_Strassen/Definitions.h
@@ -11,28 +11,42 @@
 #ifndef DEFINITIONS_H_
 #define DEFINITIONS_H_
 
+#include <tbb/scalable_allocator.h>
 #include <stdint.h>
 #include <vector>
 
 typedef uint_fast32_t M_SIZE_TYPE;		// Groessentyp der Matrizen, Schleifenzaehler usw.
 typedef double M_VAL_TYPE;				// Typ der Werte in den Matrizen (Gut: int_least32_t)
 
-#define ALL_TYPE 'S'					// Allocator-Typ
-#if ALL_TYPE == 'S'
-#include <tbb/scalable_allocator.h>
+#define ARRAY_TYPE 1
 typedef std::vector<M_VAL_TYPE, tbb::scalable_allocator<M_VAL_TYPE> > InnerArray;
+#if ARRAY_TYPE == 2
 typedef std::vector<InnerArray, tbb::scalable_allocator<InnerArray> > Matrix;
-#elif ALL_TYPE == 'C'
-#include <tbb/cache_aligned_allocator.h>
-typedef std::vector<M_VAL_TYPE, tbb::cache_aligned_allocator<M_VAL_TYPE> > InnerArray;
-typedef std::vector<InnerArray, tbb::cache_aligned_allocator<InnerArray> > Matrix;
 #else
-typedef std::vector<M_VAL_TYPE> InnerArray;
-typedef std::vector<InnerArray> Matrix;
+struct Matrix {
+	const M_SIZE_TYPE& n;
+    InnerArray mdArray;
+
+	M_SIZE_TYPE size() const _GLIBCXX_NOEXCEPT {
+		return n;
+	}
+
+	Matrix(const M_SIZE_TYPE& _n, const InnerArray) : n(_n) {
+		mdArray.resize(n * n);
+	}
+
+	M_VAL_TYPE* operator[](const M_SIZE_TYPE& row) {
+    	return &mdArray[row * n];
+    }
+
+    const M_VAL_TYPE* operator[](const M_SIZE_TYPE& row) const {
+    	return &mdArray[row * n];
+    }
+};
 #endif
 
 #define USE_PARTITIONS 1				// Aktiviert partitionierte Strassen-Algorithmen (bspw. Half-And-Half)
-#define DEBUG 0							// Debuggen? (Z. B. Verwendung von Consolen-Ausgaben, Konstanten Werten usw.)
+#define DEBUG 1							// Debuggen? (Z. B. Verwendung von Consolen-Ausgaben, Konstanten Werten usw.)
 #define MAX_RAND_VAL RAND_MAX / 50		// Zufallszahlen bis (21474836472147483647 / X) z.B. 50 oder 750
 #define STD_WIDTH 9						// Matrixausgabe: Indexbreite
 #define STD_PRECISION 5					// Matrixausgabe: Genauigkeit bei Gleitkommawerten
